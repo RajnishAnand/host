f058a690a7860fcdbb0d0cb932f8c1f4faa1389c
rajnish000anand <rajnish000anand@gmail.com>
2021-04-21 09:38:09 +0530

    .update

--- sw-testing/main.js
+++ sw-testing/main.js
@@ -2,8 +2,8 @@ if('serviceWorker' in navigator){
   window.onload=()=>{
     navigator.serviceWorker
       .register('./sw.js')
-      .then(reg=>console.log('sw-registered'))
-      .catch(err=>console.log(err));
+      .then(reg=>alert('sw-registered'))
+      .catch(err=>alert(err));
   }
 } 
  
--- sw-testing/sw.js
+++ sw-testing/sw.js
@@ -7,13 +7,13 @@ const cacheAssets=[
 
 //sw install
 self.addEventListener('install',e=>{
-  console.log('sw-installed');
+  alert('sw-installed');
   
   e.waitUntil(
     caches
       .open(cacheName)
       .then(cache=>{
-        console.log('sw-catching-files');
+        alert('sw-catching-files');
         cache.addAll(cacheAssets);
       })
       .then(_=>self.skipWaiting())
@@ -22,14 +22,14 @@ self.addEventListener('install',e=>{
 
 //sw activate
 self.addEventListener('activate',e=>{
-  console.log('Sw-activated')
+  alert('Sw-activated')
   //remove old caches
   e.waitUntil(
     caches.keys().then(cacheName=>{
       return Promise.all(
         cacheName.map(cache=>{
           if(cache!=cacheName){
-            console.log('sw-clearing-cache')
+            alert('sw-clearing-cache')
             return caches.delete(cache);
           };
         })
@@ -39,7 +39,7 @@ self.addEventListener('activate',e=>{
 });
 
 self.addEventListener('fetch',e=>{
-  console.log('sw-fectching');
+  alert('sw-fectching');
   e.respondWith(
     fetch(e.request).catch(()=>{
       caches.match(e.request);
